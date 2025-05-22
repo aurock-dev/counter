34dd0a3324bafcdcb3a5f442ea421a29d5fd752a
jdussol <jdussol@ct2mc.com>
2025-05-22 15:57:04 +0200

    add upload test

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,17 @@ const test_auth = ref([])
 const errorMsg = ref(null)
 const email = ref('jeremy24200@gmail.com')
 const password = ref('passwordtest')
+const signedUrl = ref(null)
+const uploadUrl = ref(null)
+const user = ref(null)
 
 const login = async () => {
     const { data, error: authError } = await supabase.auth.signInWithPassword({
         email: email.value,
         password: password.value
     })
+
+    const user = supabase.auth.getUser()
 
     if (authError) {
         error.value = authError.message
@@ -25,35 +30,60 @@ const login = async () => {
 
 const logout = async () => {
     await supabase.auth.signOut()
+    console.log('Déconnecté !')
+}
+
+const getImage = async () => {
+    const { data: test_bucket, error_3 } = await supabase
+        .storage
+        .from('test')
+        .createSignedUrl('images/test_480.png', 60)
+
+    console.log(test_bucket)
+
+    if (!error_3) {
+        console.log('ok')
+        signedUrl.value = test_bucket.signedUrl
+        console.log('URL temporaire :', signedUrl)
+    }
+}
+
+const uploadImage = async () => {
+    const file = event.target.files[0]
+    if (!file) return
+    const filePath = `uploads/${Date.now()}-${file.name}`
+
+    const { dataUser } = await supabase.auth.getUser()
+    user.value = dataUser.user
+    if (!user.data.user) {
+        console.log("Non connecté, upload impossible.")
+        return
+    }
+
+    const { data, error } = await supabase.storage
+        .from('test')
+        .upload(filePath, file)
 }
 
 onMounted(async () => {
-    // login()
-    // logout()
-    let { data: test, error } = await supabase
-        .from('test')
-        .select('*')
+    const { data } = await supabase.auth.getUser()
+    user.value = data.user
 
-    let { data: test_auth, error_2 } = await supabase
-        .from('test_auth')
-        .select('*')
-
-    if (error) {
-        errorMsg.value = error.message
-    }
-    else if (error_2) {
-        errorMsg.value = error_2.message
-    }
-    else {
-        test.value = test
-        test_auth.value = test_auth
-        console.log(test.value)
-        console.log(test_auth.value)
-    }
+    supabase.auth.onAuthStateChange((_event, session) => {
+        user.value = session?.user ?? null
+    })
 })
+
 </script>
 
 <template>
+    <button v-if="!user" @click="login">login</button>
+    <span v-else>
+        <img :src="signedUrl" alt="Image protégée" />
+        <button @click="logout">logout</button>
+        <button @click="getImage">getImage</button>
+        <input type="file" @change="uploadImage" />
+    </span>
     <Header />
     <RouterView />
 </template>
