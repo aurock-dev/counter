c2c2b8ec807f8df952de65ffe547cb9e279ee249
jdussol <jdussol@ct2mc.com>
2025-05-23 09:49:56 +0200

    test chartjs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,45 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue';
 import { supabase } from '@/supabase'
+
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+import { shuffle } from 'lodash'
+
+
+Chart.register(...registerables);
+
+const data = ref([30, 40, 60, 70, 5])
+const doughnutRef = ref(null)
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Doughnut Chart',
+        },
+    },
+}
+
+const testData = computed(() => ({
+    labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+    datasets: [
+        {
+            data: data.value,
+            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+        },
+    ],
+}))
+
+function shuffleData() {
+  data.value = shuffle(data.value)
+}
 
 const test = ref([])
 const test_auth = ref([])
@@ -104,6 +141,10 @@ onMounted(async () => {
         <button @click="sendDb">sendDb</button>
         <input type="file" @change="uploadImage" />
     </span>
+    <div>
+        <DoughnutChart ref="doughnutRef" :chartData="testData" :options="options" />
+        <button @click="shuffleData">Shuffle</button>
+    </div>
     <Header />
     <RouterView />
 </template>
