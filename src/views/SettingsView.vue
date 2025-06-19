8102519853ab0ab94ee28c059640888aad7f04a1
jdussol <jdussol@ct2mc.com>
2025-06-19 15:27:03 +0200

    update settings UI

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,23 +1,32 @@
 <script setup>
 import { useSettingsStore } from '@/store/settings'
+import { StretchHorizontal, Square } from 'lucide-vue-next';
+
 
 const settings = useSettingsStore()
 
 const changeWinnerColor = (option) => {
     settings.optionWinnerColor = option;
 }
-
 </script>
 
 <template>
-    <div class="settings">
-        <div class="settings__winnercolor">
-            <p>Display winner(s) color(s) :</p>
-            <div class="settings__winnercolor_options">
-                <button :class="{ '--btn-selected': settings.optionWinnerColor === 1 }"
-                    @click="changeWinnerColor(1)">Header/Footer</button>
-                <button :class="{ '--btn-selected': settings.optionWinnerColor === 2 }"
-                    @click="changeWinnerColor(2)">Everywhere</button>
+    <div class="settings" :class="[settings.optionWinnerColor === 1 ? 'settings-accentBlack' : 'settings-accentWhite']">
+        <div class="settings__section">
+            <span class="settings__title">Display winner color :</span>
+            <div class="settings__toggle-group">
+                <div class="settings__toggle"
+                    :class="{ 'settings__toggle--selected': settings.optionWinnerColor === 1 }"
+                    @click="changeWinnerColor(1)">
+                    <StretchHorizontal />
+                    <span>Header/Footer</span>
+                </div>
+                <div class="settings__toggle"
+                    :class="{ 'settings__toggle--selected': settings.optionWinnerColor === 2 }"
+                    @click="changeWinnerColor(2)">
+                    <Square />
+                    <span>Everywhere</span>
+                </div>
             </div>
         </div>
     </div>
@@ -26,25 +35,54 @@ const changeWinnerColor = (option) => {
 <style scoped>
 .settings {
     height: calc(100vh - var(--footer-height));
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 10px;
+    padding: 20px;
 }
 
-.settings__winnercolor {
+.settings-accentBlack{
+    color: var(--clr-black);
+}
+
+.settings-accentBlack .settings__toggle--selected{
+    border: 2px solid var(--clr-black);
+}
+
+.settings-accentWhite{
+    color: var(--clr-white);
+}
+
+.settings-accentWhite .settings__toggle--selected{
+    border: 2px solid var(--clr-white);
+}
+
+.settings__section {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    border-radius: 10px;
+    padding: 10px;
+    backdrop-filter: blur(40px) brightness(1.1);
+    box-shadow: var(--counter-shadow);
 }
 
-.settings__winnercolor_options {
+.settings__title {
+    font-size: 18px;
+    font-weight: 600;
+}
+
+.settings__toggle-group {
     display: flex;
     flex-direction: row;
-    justify-content: space-evenly;
+    justify-content: space-around;
+    gap: 10px;
 }
 
-.--btn-selected {
-    background-color: var(--clr-blue-700);
+.settings__toggle {
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
 }
 </style>
