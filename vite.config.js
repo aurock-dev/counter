1e79431f9471f92421b788984f7d6726b86adac0
jdussol <jdussol@ct2mc.com>
2025-06-23 11:29:49 +0200

    remove PWA, add capacitor

--- vite.config.js
+++ vite.config.js
@@ -2,35 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
     base: './',
     plugins: [
         vue(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Colorful Counter',
-                short_name: 'C#',
-                start_url: '/',
-                display: 'standalone',
-                icons: [
-                    {
-                        src: 'icons/icon-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                    },
-                    {
-                        src: 'icons/icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                    }
-                ]
-            }
-        })
     ],
     resolve: {
         alias: {
